9221debe2ce8da9fc0dde4a268dff80cb98726c1
Sandeep Sharma <sandeepsharma3530@gmail.com>
2022-08-31 09:38:41 +0530

    test and code for non zero guessed words

--- src/guessed-words/GuessedWords.js
+++ src/guessed-words/GuessedWords.js
@@ -7,6 +7,27 @@ const GuessedWords = ({ guessedWords }) => {
     content = (
       <span data-test="guess-instructions">Try to guess the secret word!</span>
     );
+  } else {
+    const guessedWordsRows = guessedWords.map((word, index) => (
+      <tr data-test="guessed-word" key={index}>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ));
+    content = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsRows}</tbody>
+        </table>
+      </div>
+    );
   }
   return <div data-test="component-guessed-words">{content}</div>;
 };
