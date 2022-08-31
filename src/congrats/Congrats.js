61c4d472da2346725c631ba5d20060f1c0e02939
Sandeep Sharma <sandeepsharma3530@gmail.com>
2022-08-31 08:14:52 +0530

    Add congrats component and utils

---
+++ src/congrats/Congrats.js
@@ -0,0 +1,13 @@
+export default function Congrats({ success }) {
+  if (success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
+}
---
+++ src/congrats/Congrats.test.js
@@ -0,0 +1,32 @@
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@cfaester/enzyme-adapter-react-18";
+
+import Congrats from "./Congrats";
+import { findByTestAttr } from "../../test/testUtils";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const setup = (props = {}) => {
+  return shallow(<Congrats {...props} />);
+};
+
+test("render without error", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-congrats");
+
+  expect(component.length).toBe(1);
+});
+
+test("renders no text when `success` props is false", () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, "component-congrats");
+
+  expect(component.text()).toBe("");
+});
+
+test("renders non-empty congrats message when `success` prop is true", () => {
+  const wrapper = setup({ success: true });
+  const message = findByTestAttr(wrapper, "congrats-message");
+
+  expect(message.text().length).not.toBe(0);
+});
